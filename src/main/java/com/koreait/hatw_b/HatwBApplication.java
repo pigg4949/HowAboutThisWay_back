0e43b7ea80173d0b0c8dcaab402be67d68184954
이솔비 <dmddnjs77@gmail.com>
2025-06-21 16:49:33 +0900

    hahaha

---
+++ src/main/java/com/koreait/hatw_b/HatwBApplication.java
@@ -0,0 +1,13 @@
+package com.koreait.hatw_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatwBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatwBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koreait/hatw_b/HatwBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.hatw_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatwBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
